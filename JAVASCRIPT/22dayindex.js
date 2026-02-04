153cc6ddf1eb4a1daa2de39fd4b55ad0fde0ee99
Divanshu-web <jeevanchaudhary.76129@gmail.com>
2026-02-04 10:48:36 +0530

    Javascript

---
+++ JAVASCRIPT/22dayindex.js
@@ -0,0 +1,10 @@
+var person_name = "Divanshu"
+console.log(person_name)
+
+var num1 = 200
+var num2 = 100
+
+console.log("The Addition of ", num1, "and", num2, "is",num1 + num2)
+console.log("The Subtraction of ", num1, "and", num2, "is",num1 - num2)
+console.log("The Multiplication of ", num1, "and", num2, "is",num1 * num2)
+console.log("The Division of ", num1, "and", num2, "is",num1 / num2)
