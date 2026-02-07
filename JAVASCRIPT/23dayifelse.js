0f86459cc6b2dfb580e19cea014f7a0c81595e14
Divanshu-web <jeevanchaudhary.76129@gmail.com>
2026-02-07 09:14:30 +0530

    responsive and javascript

---
+++ JAVASCRIPT/23dayifelse.js
@@ -0,0 +1,68 @@
+// if(condition){
+//   // body
+// }
+
+
+// var age = 20;
+
+// if(age >= 18){
+//  console.log("You are eligible");
+ 
+// }else{
+// console.log("You are not elligible");
+// }
+var yourage = 20;
+if(yourage>=18){
+    console.log("you are eligible");
+}else{
+    console.log("You are not eligible");
+}
+
+
+// if(condition){
+
+// }else if(condition){
+
+// }else{
+
+// }
+
+// let marks = 40;
+
+// if(marks > 90){
+//   console.log("Grade A");
+// }else if(marks > 70){
+//   console.log("Grade B");
+  
+// }else if(marks > 50){
+//    console.log("Grade C");
+// }
+
+let yourmarks = 50;
+
+if(yourmarks>90)
+    {
+    console.log("Excellent");
+}
+ else if(yourmarks > 50)
+{
+    console.log("Good");
+
+}else if(yourmarks< 20)
+{
+    console.log("Fail");
+}
+
+let age = "18";
+let hasId = true;
+if(age !== 18){
+  if(hasId){ 
+    console.log("You can vote!!");
+  }else{
+  console.log("You cannot vote!!");
+  }
+}else{
+  console.log("You cannot vote!!");
+}
+
+
